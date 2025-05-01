bb75ccb609a4e51499bbd05f2fb33cff4383364a
rezk <ahmedrezkofficial@gmail.com>
2025-05-01 11:57:32 +0300

    refactor main file to reusablility

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ from src.data_preprocessing import load_data, preprocess_data, log_processed_dat
 from src.visualization import *
 from src.model import split_data, train_all_models, plot_roc_curves
 
-# 1. Load Data
-df = load_data(DATA_PATH)
+def main():
+    # 1. Load Data
+    df = load_data(DATA_PATH)
 
-# 2. Visualize before processing
-plot_churn_distribution(df)
-plot_tenure_vs_churn(df)
-plot_monthly_charges(df)
-plot_correlation_heatmap(df)
+    # 2. Visualize before processing
+    plot_churn_distribution(df)
+    plot_tenure_vs_churn(df)
+    plot_monthly_charges(df)
+    plot_correlation_heatmap(df)
 
-# 3. Preprocess
-X, y = preprocess_data(df)
-log_processed_data(df, PROCESSED_DATA_PATH)    # log_processed_data(X, y)
+    # 3. Preprocess
+    X, y = preprocess_data(df)
+    log_processed_data(X, y)  # log_processed_data(X, y)
 
+    # 4. Train-test split
+    X_train, X_test, y_train, y_test = split_data(X, y)
 
-# 4. Train-test split
-X_train, X_test, y_train, y_test = split_data(X, y)
+    # 5. Train and evaluate models
+    models = train_all_models(X_train, X_test, y_train, y_test)
 
-# 5. Train and evaluate models
-models = train_all_models(X_train, X_test, y_train, y_test)
+    # 6. Plot ROC-AUC
+    plot_roc_curves(models, X_test, y_test)
 
-# 6. Plot ROC-AUC
-plot_roc_curves(models, X_test, y_test)
+if __name__ == "__main__":
+    main()
